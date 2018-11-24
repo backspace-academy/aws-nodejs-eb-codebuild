3ffa8bd7deb245512c7edc461414795b506f8d7f
BackSpace Academy <31625808+backspace-academy@users.noreply.github.com>
2018-11-25 00:28:59 +1100

    .

---
+++ app.js
@@ -0,0 +1,27 @@
+// Load the AWS SDK
+var AWS = require('aws-sdk')
+
+var express = require('express')
+var bodyParser = require('body-parser')
+
+// Set region for AWS SDKs
+AWS.config.region = process.env.REGION
+
+var app = express()
+
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: 'BackSpace Academy & Elastic Beanstalk'
+    })
+    res.status(200).end();
+})
+
+var port = process.env.PORT || 3000
+
+var server = app.listen(port, function () {
+  console.log('Server running at http://127.0.0.1:' + port + '/')
+})
